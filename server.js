3d723c3f0b4464f7a03cda92379c6dd840c4577c
astribli <andrewjstribling@gmail.com>
2020-11-09 20:16:54 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.get('/helloworld', (req, res) => {
+    res.send("Hello World")
+});
+
+
+app.listen(PORT, () => {
+    console.log('Server listening on: http://localhost:' + PORT);
+  });
