637f3e66fb14345a426c42253924ef4fd7913f44
Max Plumer <68719068+mplumer@users.noreply.github.com>
2020-11-10 19:17:26 -0500

    add html

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-//const exphbs = require('express-handlebars');
-//const helpers = require('./utils/helpers');
-//const hbs = exphbs.create({ helpers });
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
@@ -21,8 +21,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
-//app.engine('handlebars', hbs.engine);
-//app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
