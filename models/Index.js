b2ccdd8a6c78256228c8e4912b7b616e1a9b4fbd
QuaggWasTaken <forrestfaulkner1@gmail.com>
2020-11-11 20:44:54 -0600

    Preliminary Comment Commit

--- models/Index.js
+++ models/Index.js
@@ -1,5 +1,7 @@
 const User = require("./User")
 const Pin = require("./Pin")
+const Comment = require('./Comment')
+//const Picture = require('./Picture')
 
 User.hasMany(Pin, {
     foreignKey: "user_id"
@@ -9,4 +11,12 @@ Pin.belongsTo(User, {
     foreignKey: "user_id",
 });
 
-module.exports = { User, Pin, }
+Pin.hasOne(Comment, {
+    foreignKey: "pin_id"
+});
+
+Comment.belongsTo(Pin, {
+    foreignKey: 'pin_id'
+})
+
+module.exports = { User, Pin, Comment, }
